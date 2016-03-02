39c56ad78e314baf960569754f9f7dd8d8dcb09e
James Heywood <jdheywood@gmail.com>
2016-03-02 23:14:11 +0000

    Playing around with nav and structure

--- app/routes/index.js
+++ app/routes/index.js
@@ -25,8 +25,8 @@ module.exports = function (app) {
     
     app.get('/contact', function (req, res) {
         res.render('contact', {
-            'carouselItems': [ 
-                { 'id': 0, 'src': '/img/spa-1.jpg', 'alt': 'spa 1', 'heading': 'Lorem ipsum', 'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'linkUrl': '#', 'linkText': 'Call to action' }, 
+            'carouselItems': [
+                { 'id': 0, 'src': '/img/spa-1.jpg', 'alt': 'spa 1', 'heading': 'Lorem ipsum', 'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 'linkUrl': '#', 'linkText': 'Call to action' },
                 { 'id': 1, 'src': '/img/spa-2.jpg', 'alt': 'spa 2', 'heading': '', 'text': 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'linkUrl': '#', 'linkText': 'Do something' },
                 { 'id': 2, 'src': '/img/spa-4.jpg', 'alt': 'spa 4', 'heading': '', 'text': 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur', 'linkUrl': '#', 'linkText': 'Or not wevs' },
                 { 'id': 3, 'src': '/img/woman.jpg', 'alt': 'woman', 'heading': '', 'text': 'Woman in the picture who are you?', 'linkUrl': '#', 'linkText': 'Hey click me' }
