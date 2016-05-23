2f66ca7ef96c1d7d7a0ffbdb2d2c7e9e070dff40
James Heywood <jdheywood@gmail.com>
2016-05-23 14:21:05 +0100

    added blog to navigation, started re-structure of blog index page

--- app/models/post.js
+++ app/models/post.js
@@ -4,11 +4,16 @@ var Schema = mongoose.Schema;
 // Document schema in MongoDB
 var PostSchema = new Schema({
   title: String,
+  stub: String,
   order: Number,
   author: String,
   created: Date,
   body: String
 });
+
+PostSchema.methods.url = function() {
+    return this.title.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-');
+};
 
 // We can now call mongoose.model('Post') in our code
 var Post = mongoose.model('Post', PostSchema);
--- app/routes/blog.js
+++ app/routes/blog.js
@@ -5,17 +5,18 @@ module.exports = function (app) {
 
     app.get('/blog', function (req, res) {
         var Post = require('../models/post');
-        Post.count({}, function (error, count) {
+        Post.find({}, function (error, posts) {
             res.render('blog-index', {
                 title: 'Bear Essentials Massage - Blog',
-                heading: 'Blog',
-                subtitle: 'Lorem ipsum dolor sit amet',
-                bodyText: 'Lorem ipsum dolor sit amet',
+                heading: 'Bear Essentials Blog',
+                subtitle: 'Welcome to our blog',
+                bodyText: 'Welcome to the Bear Essentials Massage Blog, a place where you can keep up to date on all that is happening with our little company and learn all about what we do, the treatments we offer and what makes us tick.',
                 imageClass: 'blog',
                 pageName: 'blog',
                 keywords: 'Blog, Bear Essentials Massage, News, Updates, Information, Offers, Brighton, Hove, Treatment, Therapy, Sports Massage, Sports Taping, Hot Stone Massage Pregnancy Massage, Seated Acupressure',
                 description: 'Bear Essentials Massage blog, the place to come for the latest news and offers from Bear Essentials Massage.',
-                numberOfBlogPosts: count
+                numberOfBlogPosts: posts.length,
+                posts: posts
             });
         });
     });
@@ -25,7 +26,8 @@ module.exports = function (app) {
         Post.findOne({'order': 1}, function (error, post) {
             res.render('blog-post', {
                 title: 'Bear Essentials Massage - Blog',
-                heading: 'Blog',
+                url: post.url(),
+                heading: 'Bear Essentials Blog',
                 imageClass: 'blog',
                 subtitle: post.title,
                 bodyText: post.body
